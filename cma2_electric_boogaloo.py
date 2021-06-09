f3844efe35659e3c115906c73fdb66044a5bbc59
Adam Grabski <adam.gr@outlook.com>
2021-06-09 23:23:59 +0200

    steaming pile of garbage

--- cma2_electric_boogaloo.py
+++ cma2_electric_boogaloo.py
@@ -24,19 +24,20 @@ def weighted_neighbour_avg_discarding_cma(fitness_function , initial_solution, i
 				new_generation = {};
 				all_previous_specimen = list(self._previous_generation.keys());
 				for elem in x:
-					set = [elem.numpy()];
+					set = [tuple(elem.numpy())];
 					for specimen in all_previous_specimen:
 						set.append(specimen);
 					nbrs = NearestNeighbors(n_neighbors=k).fit(set);
 					distances, neighbours = nbrs.kneighbors();
 					neighbour_indicies = neighbours[0];
+					neighbour_indicies = [index for index in neighbour_indicies if tuple(elem.numpy()) != set[index]]
 					evaluations = [self._previous_generation[tuple(list(set[i]))] for i in neighbour_indicies];
 					variance = statistics.variance(evaluations);
 
 					if variance > self._avg_previous_variance * v:
 						new_generation[tuple(elem.numpy())] = self._fitness(elem).numpy();
 					else:
-						weighted_evals = [evaluations[i] * distances[0][index - 1] for i, index in enumerate(neighbour_indicies)];
+						weighted_evals = [self._previous_generation[tuple(list(set[index]))] * distances[0][i - 1] for i, index in enumerate(neighbour_indicies)];
 
 						new_generation[tuple(elem.numpy())] = sum(weighted_evals) / sum([distances[0][index - 1] for _, index in enumerate(neighbour_indicies)])
 
@@ -57,12 +58,48 @@ def weighted_neighbour_avg_discarding_cma(fitness_function , initial_solution, i
 
 
 
-def low_variance_neighborhood_average_cma(fitness_function, initial_solution, initial_step_size):
-	def real_fitness_function(x):
-		return fitness_function(3);
+def low_variance_neighborhood_average_cma(fitness_function, initial_solution, initial_step_size, k, s, population_size):
+	class real_fitness_function:
+		def __init__(self):
+			self._fitness = fitness_function;
+			self._previous_generation = None;
+
+		def __call__(self, x: Tensor) -> Tensor:
+			if(self._previous_generation == None):
+				self._previous_generation = {};
+				for elem in x:
+					self._previous_generation[tuple(elem.numpy())] = self._fitness(elem).numpy();
+				return tf.convert_to_tensor(list(self._previous_generation.values()));
+
+			else:
+				previous_median = statistics.median(list(self._previous_generation.values()))
+				new_generation = {};
+				all_previous_specimen = list(self._previous_generation.keys());
+				for elem in x:
+					set = [elem.numpy()];
+					for specimen in all_previous_specimen:
+						set.append(specimen);
+					nbrs = NearestNeighbors(n_neighbors=k).fit(set);
+					distances, neighbours = nbrs.kneighbors();
+					neighbour_indicies = neighbours[0];
+					neighbour_indicies = [index for index in neighbour_indicies if tuple(elem.numpy()) != set[index]]
+
+					weighted_evals = [self._previous_generation[tuple(list(set[index]))] * distances[0][i - 1] for i, index in enumerate(neighbour_indicies)];
+					weighted_evals = [e for e in weighted_evals if np.isfinite(e)]
+					weighted_avg_eval = sum(weighted_evals) / sum([distances[0][i] for i, index in enumerate(neighbour_indicies)]);
+					if weighted_avg_eval > previous_median * s:
+						new_generation[tuple(elem.numpy())] = self._fitness(elem).numpy();
+					else:
+						new_generation[tuple(elem.numpy())] = float('-inf');
+
+
+				result = tf.convert_to_tensor(list(new_generation.values()));
+				self._previous_generation = new_generation;
+				return result;
 	
 	return CMA(
 		initial_solution,
 		initial_step_size,
-		real_fitness_function);
+		real_fitness_function(),
+		population_size=population_size);
 	
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from cma2_electric_boogaloo import weighted_neighbour_avg_discarding_cma
+from cma2_electric_boogaloo import weighted_neighbour_avg_discarding_cma, low_variance_neighborhood_average_cma
 
 def fitness_fn(x):
 	return (
@@ -15,6 +15,18 @@ cma = weighted_neighbour_avg_discarding_cma(
 	k=10,
 	v=1,
 )
+
+cma2 = low_variance_neighborhood_average_cma(
+	fitness_fn,
+	initial_solution=[1.5, -0.4],
+	initial_step_size=1.0,
+	k=10,
+	s=1,
+	population_size=100
+)
 best_solution, best_fitness = cma.search()
+best_solution2, best_fitness2 = cma2.search()
 print(best_solution)
 print(best_fitness)
+print(best_solution2)
+print(best_fitness2)
